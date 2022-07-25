ec02c3934ebeea77dcfdfed34d95ce8a7bf49e49
fadhlillahrizky <rizky.fadhlillah3@gmail.com>
2022-07-25 07:41:09 +0700

    feat: kosongan

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,4 +1,10 @@
 const express = require("express")
 const mainRouter = express.Router()
+const book = require('./book');
+const member = require('./member');
+
+
+mainRouter.use('/book', book)
+mainRouter.use('/member', member)
 
 module.exports = mainRouter
