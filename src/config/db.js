4274dbd232376295149b43c8861cc96d7f89a10b
fadhlillahrizky <rizky.fadhlillah3@gmail.com>
2022-08-16 13:54:19 +0700

    fix: dialect

--- src/config/db.js
+++ src/config/db.js
@@ -6,7 +6,7 @@ const db = new  Sequelize(
   process.env.DB_PASSWORD,
   {
       host: process.env.DB_HOST,
-      dialect: 'mysql',
+      dialect: process.env.DB_DIALECT ?? 'mysql',
       port: process.env.DB_PORT,
       pool: {
           max: 50,
